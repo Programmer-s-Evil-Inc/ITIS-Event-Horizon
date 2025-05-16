184fe6a151255151cc136526a08c651cda5d3908
Shozha <sergeyandreeiav20@gmail.com>
2025-05-17 02:31:52 +0300

    add POST /event/{eventId}/subscribe endpoint for event subscription

---
+++ src/main/java/ru/kpfu/itis/webapp/controller/ExceptionHandlerController.java
@@ -0,0 +1,23 @@
+package ru.kpfu.itis.webapp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import ru.kpfu.itis.webapp.exceptions.DataNotFoundException;
+import ru.kpfu.itis.webapp.exceptions.ServiceException;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler({DataNotFoundException.class})
+    public ResponseEntity<String> handleNotFound(Exception ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler({ServiceException.class})
+    public ResponseEntity<String> handleConflict(Exception ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+}
